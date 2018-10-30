0d6cd89ae38eedf8a0d99fc525a3699b8c5f114b
wangronfa@gmail.com <wangronfa@gmail.com>
2018-10-30 15:08:13 +0800

    Discard程序

---
+++ dotNettyCoreDemo/Discard.Client/DiscardClientHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using DotNetty.Buffers;
+using DotNetty.Transport.Channels;
+using Examples.Common;
+
+namespace Discard.Client
+{
+    public class DiscardClientHandler : SimpleChannelInboundHandler<object>
+    {
+        IChannelHandlerContext ctx;
+        byte[] array;
+
+        public override void ChannelActive(IChannelHandlerContext ctx)
+        {
+            this.array = new byte[ClientSettings.Size];
+            this.ctx = ctx;
+
+            // Send the initial messages.
+            this.GenerateTraffic();
+        }
+
+        protected override void ChannelRead0(IChannelHandlerContext context, object message)
+        {
+            // Server is supposed to send nothing, but if it sends something, discard it.
+        }
+
+        public override void ExceptionCaught(IChannelHandlerContext ctx, Exception e)
+        {
+            Console.WriteLine("{0}", e.ToString());
+            this.ctx.CloseAsync();
+        }
+
+        async void GenerateTraffic()
+        {
+            try
+            {
+                IByteBuffer buffer = Unpooled.WrappedBuffer(this.array);
+                // Flush the outbound buffer to the socket.
+                // Once flushed, generate the same amount of traffic again.
+                await this.ctx.WriteAndFlushAsync(buffer);
+                this.GenerateTraffic();
+            }
+            catch
+            {
+                await this.ctx.CloseAsync();
+            }
+        }
+    }
+}
---
+++ dotNettyCoreDemo/Discard.Server/DiscardServerHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using DotNetty.Transport.Channels;
+
+namespace Discard.Server
+{
+    public class DiscardServerHandler : SimpleChannelInboundHandler<object>
+    {
+        protected override void ChannelRead0(IChannelHandlerContext context, object message)
+        {
+        }
+
+        public override void ExceptionCaught(IChannelHandlerContext ctx, Exception e)
+        {
+            Console.WriteLine("{0}", e.ToString());
+            ctx.CloseAsync();
+        }
+    }
+}
